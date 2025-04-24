85bd35e15c33791cec65a8c2ca606c75b85c6a41
erumhussain110 <erumaijaz1915@gmail.com>
2025-04-24 06:04:37 -0700

    Add files via upload

---
+++ task_4.js
@@ -0,0 +1,15 @@
+
+
+let num1=20;
+let num2=30;
+
+let sum=num1+num2;
+console.log("Addition"  + ":" +   num1 +  "+"  +  num2  + "=" +sum )
+
+let sub=num1-num2;
+console.log("subtraction"  + ":" +   num1 +  "-"  +  num2  + "=" +sub)
+
+
+let multi=num1*num2;
+console.log("multiplication"  + ":" +   num1 +  "*"  +  num2  + "=" +multi )
+
